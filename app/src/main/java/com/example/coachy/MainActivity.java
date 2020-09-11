038f2e483d6bb483ea1082d352c2b623a6519fa7
ShonOhana <61653311+ShonOhana@users.noreply.github.com>
2020-09-11 20:18:25 +0300

    Firebase add

--- app/src/main/java/com/example/coachy/MainActivity.java
+++ app/src/main/java/com/example/coachy/MainActivity.java
@@ -22,8 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        int eden = 0;
-        int shon = 100;
+        String firebaseRock;
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
