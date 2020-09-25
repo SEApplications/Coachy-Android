c90879e2c71aeb768fc532c414a4f51061d193aa
ShonOhana <61653311+ShonOhana@users.noreply.github.com>
2020-09-25 21:02:44 +0300

    Search coaches

--- app/src/main/java/com/example/coachy/adapters/DetailsTrainingAdapter.java
+++ app/src/main/java/com/example/coachy/adapters/DetailsTrainingAdapter.java
@@ -1,16 +1,23 @@
 package com.example.coachy.adapters;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.coachy.R;
 import com.example.coachy.models.Coach;
 import com.example.coachy.models.TrainingType;
+import com.example.coachy.models.UploadFirebase;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
+import java.util.Random;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -19,7 +26,6 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class DetailsTrainingAdapter extends RecyclerView.Adapter<DetailsTrainingAdapter.ViewHolder>  {
 
-    //for the images
     private List<Coach> mDataSet;
     private Context context;
 //    private TrainingTypeAdapter.OnClickSelected callback;
@@ -49,13 +55,17 @@ public class DetailsTrainingAdapter extends RecyclerView.Adapter<DetailsTraining
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
      //   holder.trainingType.setImageResource(mDataSet.get(position).getTrainingImage());
 
- //       Random rnd = new Random();
- //       int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
- //       holder.playVideoBtn.setBackgroundTintList(color);
+        Random rnd = new Random();
+        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        holder.playVideoBtn.setBackgroundTintList(ColorStateList.valueOf(color));
 
         holder.cardView.setOnClickListener(b->{
            // callback.onTrainingTypeSelected(mDataSet.get(position));
         });
+
+        Picasso.get().load(mDataSet.get(position).getProfileImage()).placeholder(R.drawable.aerobic).into(holder.coachProfile);
+        holder.coachName.setText(mDataSet.get(position).getFullName());
+
     }
 
     @Override
@@ -74,12 +84,14 @@ public class DetailsTrainingAdapter extends RecyclerView.Adapter<DetailsTraining
         private CircleImageView coachProfile;
         private CardView cardView;
         private FloatingActionButton playVideoBtn;
+        private TextView coachName;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             coachProfile = itemView.findViewById(R.id.im_coach_profile);
             cardView = itemView.findViewById(R.id.parent_layout);
             playVideoBtn = itemView.findViewById(R.id.fab_play);
+            coachName = itemView.findViewById(R.id.tv_coach_name);
         }
     }
 
